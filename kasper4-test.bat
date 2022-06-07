aa745f84d7bf0dcfd1032ffad51aa4670ed0f2eb
siddhartha77 <77siddhartha@gmail.com>
2022-06-06 18:59:43 -0700

    Add files via upload

---
+++ kasper4-test.bat
@@ -0,0 +1,7 @@
+echo 12345678901234567| Kasper4.exe 191DEE4A13759F73
+echo florence| Kasper4.exe 864eed03a9b4e824
+echo thea| Kasper4.exe 0659C2E6EE79454A
+echo abcdefghijkl| Kasper4.exe f615d8ac23be32fe
+echo 12345678| Kasper4.exe 4ddabd585e0cfd55
+
+pause
